d166d1b2b2c045c3046c6591426e110b0d84ce0c
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-22 10:40:33 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,25 +2,15 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-unique = BRANDS.uniq!
+  unique = BRANDS.uniq!
 
 def initialize(brand)
     @brand = brand
-
+   BRANDS << brand
   end
 
   def cobble(size)
     self.condition = "new"
     puts "Your shoe is as good as new!"
 
-end
-def brands=(brands)
-
-    @brands = brands
-    brands = brands
-    BRANDS << brand
-  end
-def size=(size)
-  BRANDS.size = 3
-end
 end
