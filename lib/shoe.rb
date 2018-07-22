2c15377cbac18f6e4246ebddd84f5a13dffbf001
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-22 09:45:19 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,10 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS = []
+  BRANDS = ["Uggs", "Rainbow", "Nike", "Nike"]
 
   def initialize(brand)
     @brand = brand
-    brands = ["Uggs", "Rainbow", "Nike", "Nike"]
   end
 
   def cobble
